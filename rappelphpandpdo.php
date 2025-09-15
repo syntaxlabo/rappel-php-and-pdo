e47ebab4c410f9800b08c14d3d0b224cc2930b34
syntaxlabo <syntaxlabo@gmail.com>
2025-09-15 16:21:11 +0100

    Update rappelphpandpdo.php

--- rappelphpandpdo.php
+++ rappelphpandpdo.php
@@ -7,10 +7,10 @@ function buildArticle(array $row=[]): array{
     $title= isset($row['title'])?(string)$row['title']:'no title';
     $excert=isset($row['excert'])?(is_string($row['excert'])?$row['excert']:null):null;
     $views= isset($row['views'])?(is_int($row['views'])?max(0,$row['views']):0):0;
-    $published= (bool)$row['published'];
+    $published= isset($row['author'])?(bool)$row['author']:(bool)false;
     $author= isset($row['author'])?(is_string($row['author'])?trim((string)$row['author']):'N/A'):'N/A';
 
-    return [$title,$excert,$views,$published,$author];
+    return ['title'=>$title,'exxert'=>$excert,'views'=>$views,'published'=>$published,'author'=>$author];
 }
 
 $arr= buildArticle();
