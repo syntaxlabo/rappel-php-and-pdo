60d0b3807f3e2294033695c72928561060dfac33
syntaxlabo <syntaxlabo@gmail.com>
2025-09-15 16:31:38 +0100

    Update rappelphpandpdo.php

--- rappelphpandpdo.php
+++ rappelphpandpdo.php
@@ -4,13 +4,13 @@
 declare(strict_types=1);
 function buildArticle(array $row=[]): array{
 
-    $title= isset($row['title'])?(string)$row['title']:'no title';
+    $title= isset($row['title'])?(string)$row['title']:(string)'no title';
     $excert=isset($row['excert'])?(is_string($row['excert'])?$row['excert']:null):null;
-    $views= isset($row['views'])?(is_int($row['views'])?max(0,$row['views']):0):0;
+    $views= isset($row['views'])?(is_int($row['views'])?max(0,$row['views']):(int)0):(int)0;
     $published= isset($row['author'])?(bool)$row['author']:(bool)false;
-    $author= isset($row['author'])?(is_string($row['author'])?trim((string)$row['author']):'N/A'):'N/A';
+    $author= isset($row['author'])?(is_string($row['author'])?trim((string)$row['author']):(string)'N/A'):(string)'N/A';
 
-    return ['title'=>$title,'exxert'=>$excert,'views'=>$views,'published'=>$published,'author'=>$author];
+    return ['title'=>$title,'excert'=>$excert,'views'=>$views,'published'=>$published,'author'=>$author];
 }
 
 $arr= buildArticle();
