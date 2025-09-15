109f9b756decec0e6cee819be8d240e65341a186
syntaxlabo <syntaxlabo@gmail.com>
2025-09-15 16:14:28 +0100

    new function

---
+++ rappelphpandpdo.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+declare(strict_types=1);
+function buildArticle(array $row=[]): array{
+
+    $title= isset($row['title'])?(string)$row['title']:'no title';
+    $excert=isset($row['excert'])?(is_string($row['excert'])?$row['excert']:null):null;
+    $views= isset($row['views'])?(is_int($row['views'])?max(0,$row['views']):0):0;
+    $published= (bool)$row['published'];
+    $author= isset($row['author'])?(is_string($row['author'])?trim((string)$row['author']):'N/A'):'N/A';
+
+    return [$title,$excert,$views,$published,$author];
+}
+
+$arr= buildArticle();
+print_r($arr);
+
+?>
