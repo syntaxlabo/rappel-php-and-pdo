66bedfb842f5627629131a9688d913965ae6b63f
syntaxlabo <syntaxlabo@gmail.com>
2025-09-16 10:37:28 +0100

    rappelphpandpdo

--- functionettableaux.php
+++ functionettableaux.php
@@ -1,30 +1,5 @@
-<?php 
+<?php
 declare(strict_types=1);
-echo " ***********************************************";
-echo " Types & syntaxe utiles";
-echo " ***********************************************";
-
-
-
-
-function buildArticle(array $row=[]): array{
-
-    $title= isset($row['title'])?(string)$row['title']:(string)'no title';
-    $excert=isset($row['excert'])?(is_string($row['excert'])?$row['excert']:null):null;
-    $views= isset($row['views'])?(is_int($row['views'])?max(0,$row['views']):(int)0):(int)0;
-    $published= isset($row['author'])?(bool)$row['author']:(bool)false;
-    $author= isset($row['author'])?(is_string($row['author'])?trim((string)$row['author']):(string)'N/A'):(string)'N/A';
-
-    return ['title'=>$title,'excert'=>$excert,'views'=>$views,'published'=>$published,'author'=>$author];
-}
-
-$arr= buildArticle();
-
-print_r($arr);
-
-
-
-
 /////////////////////////////
 
 echo " ***********************************************";
@@ -57,6 +32,7 @@ $normalize = array_map(
 usort($normalize, fn($a, $b) => $b['views'] <=> $a['views']);
 
 print_r($normalize);
+
 
 
 
