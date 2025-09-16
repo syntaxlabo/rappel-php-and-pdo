3e1b1390191fcb9bbb775aa39705e1b481692080
syntaxlab <syntaxlabo@gmail.com>
2025-09-16 15:39:12 +0100

    d

--- chapitre2/tuto.php
+++ chapitre2/tuto.php
@@ -8,14 +8,31 @@ $articles = [
   ['id'=>3,'title'=>'Composer & Autoload','category'=>'outils','views'=>90,'author'=>'Amina','published'=>false, 'tags'=>['composer','php']],
   ['id'=>4,'title'=>'Validation FormRequest','category'=>'laravel','views'=>210,'author'=>'Sara','published'=>true,  'tags'=>['laravel','validation']],
 ];
+//this function cleans the input string
+// transform all letters to lowercase 
+// replace a specefic kind of characters with regular expression
+// and returns a trimmed output of the string
 function slugify(string $title): string {
     $slug = strtolower($title);
     $slug = preg_replace('/[^a-z0-9]+/i', '-', $slug);
     return trim($slug, '-');
 }
+
+
+
+
+//filters the array articles and return only the articles with published true
+//then return a new numerical indexed array
 $published = array_values(
   array_filter($articles, fn(array $a) => $a['published'] ?? false)
 );
+
+
+
+//simplifies the published array . 
+// this function iterates throw each article 
+// and simplifies it using new keys and cleaning 
+// the slug with the previous function
 $light = array_map(
   fn(array $a) => [
     'id'    => $a['id'],
@@ -25,9 +42,20 @@ $light = array_map(
   ],
   $published
 );
+
+
+//assigning a new variable name to the light array
+// then sort it with usort
 $top = $light;
 usort($top, fn($a, $b) => $b['views'] <=> $a['views']);
+
+
+
+//returns a new sliced array 
+// with just three first elements
 $top3 = array_slice($top, 0, 3);
+
+//encriments the number of valid authors
 $byAuthor = array_reduce(
   $published,
   function(array $acc, array $a): array {
@@ -37,6 +65,8 @@ $byAuthor = array_reduce(
   },
   []
 );
+
+
 $allTags = array_merge(...array_map(fn($a) => $a['tags'], $published));
 
 $tagFreq = array_reduce(
